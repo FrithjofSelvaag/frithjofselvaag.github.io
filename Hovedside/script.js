8cc765270aeeb31eda5b296c2556e7e26fde50d5
HenrikDarre06 <henidarre@gmail.com>
2024-03-14 11:30:34 +0100

    Update script.js

--- Hovedside/script.js
+++ Hovedside/script.js
@@ -1,3 +1,5 @@
+//hamburger-meny
+
 let burgerEl = document.querySelector('.fa-chart-bar')
 
 let navEl = document.querySelector('nav')
@@ -6,4 +8,16 @@ burgerEl.addEventListener('click', showNav)
 
 function showNav() {
     navEl.classList.toggle('show')
+    selectEL.classList.toggle('show')
+}
+
+// ColorPalate
+let selectEL = document.getElementById('ColorPalate')
+let navbarEL = document.getElementById('header')
+
+
+selectEL.addEventListener("change", colorPalate())
+
+function colorPalate(){
+
 }
