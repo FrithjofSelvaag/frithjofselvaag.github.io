3d969ee0ee7c2fb46c8921df4e4e4a27bec67a8b
HenrikDarre06 <henidarre@gmail.com>
2024-03-15 19:09:57 +0100

    Update script.js

--- Hovedside/script.js
+++ Hovedside/script.js
@@ -6,11 +6,17 @@ let burgerEl = document.querySelector('.fa-chart-bar')
 
 let navEl = document.querySelector('nav')
 
+let mainDivEl = document.querySelector('.spillboks div')
+
+let mainEl = document.querySelector('.spillboks')
+
+
 burgerEl.addEventListener('click', showNav)
 
 function showNav() {
     navEl.classList.toggle('show')
     selectEl.classList.toggle('show')
+
 }
 
 // ColorPalate
@@ -19,11 +25,14 @@ let body = document.body
 let h1 = document.querySelector('h1')
 let header = document.querySelector('header')
 let chartBar = document.querySelector('.fa-chart-bar')
+let nav = document.querySelector('nav a')
+let home = document.querySelector('div a')
+let lorem = document.querySelectorAll('main div h1')
 
 
 selectEl.addEventListener("change", colorPalate)
 
-function colorPalate(value){
+function colorPalate(){
 
     let r = Math.floor(Math.random()*256)
     let g = Math.floor(Math.random()*256)
@@ -35,13 +44,19 @@ function colorPalate(value){
         h1.style.color = 'black'
         selectEl.style.backgroundColor = 'rgb(5, 163, 215)'
         chartBar.style.color = 'white'
+        lorem.forEach(function(lorem) {
+            lorem.style.color = 'black'
+        })
     }
     else if (selectEl.value === "black") {
-        body.style.backgroundColor = 'black'
-        selectEl.style.backgroundColor = 'gray'
-        header.style.backgroundColor = 'gray'
+        body.style.backgroundColor = 'rgb(17, 17, 17)'
+        selectEl.style.backgroundColor = 'black'
+        header.style.backgroundColor = 'black'
         h1.style.color = 'white'
         chartBar.style.color = 'white'
+        lorem.forEach(function(lorem) {
+            lorem.style.color = 'white'
+        })
     }
     else if (selectEl.value === "colors") {
         body.style.backgroundColor = `rgb(${r},${g},${b})`
@@ -57,5 +72,11 @@ function colorPalate(value){
         g = Math.floor(Math.random()*256)
         b = Math.floor(Math.random()*256)
         chartBar.style.color = `rgb(${g},${b},${r})`
+        r = Math.floor(Math.random()*256)
+        g = Math.floor(Math.random()*256)
+        b = Math.floor(Math.random()*256)
+        lorem.forEach(function(lorem) {
+            lorem.style.color = `rgb(${g},${b},${r})`
+        })
     }
 }
