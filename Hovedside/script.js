38c956ba98ea9a6da788685bcee072ac65983617
HenrikDarre06 <henidarre@gmail.com>
2024-03-15 09:08:14 +0100

    Update script.js

--- Hovedside/script.js
+++ Hovedside/script.js
@@ -25,19 +25,37 @@ selectEl.addEventListener("change", colorPalate)
 
 function colorPalate(){
 
+    let r = Math.floor(Math.random()*256)
+    let g = Math.floor(Math.random()*256)
+    let b = Math.floor(Math.random()*256)
+
     if (selectEl.value === "white") {
         header.style.backgroundColor = 'rgb(5, 163, 215)'
         body.style.backgroundColor = 'white'
         h1.style.color = 'black'
-        chartBar.style.color = 'white'
         selectEl.style.backgroundColor = 'rgb(5, 163, 215)'
-
+        chartBar.style.color = 'white'
     }
     else if (selectEl.value === "black") {
         body.style.backgroundColor = 'black'
         selectEl.style.backgroundColor = 'indigo'
         header.style.backgroundColor = 'indigo'
         h1.style.color = 'white'
-        
+        chartBar.style.color = 'white'
+    }
+    else if (selectEl.value === "colors") {
+        body.style.backgroundColor = `rgb(${r},${g},${b})`
+        r = Math.floor(Math.random()*256)
+        g = Math.floor(Math.random()*256)
+        b = Math.floor(Math.random()*256)
+        selectEl.style.backgroundColor = `rgb(${g},${r},${b})`
+        r = Math.floor(Math.random()*256)
+        g = Math.floor(Math.random()*256)
+        b = Math.floor(Math.random()*256)
+        header.style.backgroundColor = `rgb(${b},${g},${r})`
+        r = Math.floor(Math.random()*256)
+        g = Math.floor(Math.random()*256)
+        b = Math.floor(Math.random()*256)
+        chartBar.style.color = `rgb(${g},${b},${r})`
     }
 }
