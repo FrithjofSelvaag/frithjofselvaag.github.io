b87feb70c57db764974a9b3935011476b6249b11
FrithjofSelvaag <frstse@gmail.com>
2024-04-10 21:24:55 +0200

    kokte til det ikke var mer ingredienser

--- visual-memory-game/script.js
+++ visual-memory-game/script.js
@@ -21,45 +21,41 @@ må også lage skjerm jeg kommer til ved tap - kan kopiere litt fra henrik her.
 
 let bokserEls=document.querySelectorAll('div div')
 let buttonEl=document.querySelector('#StartKnapp')
-console.log(bokserEls.length)
+console.log(`lengde på bokserEls array: ${bokserEls.length}`)
+console.log(`bokserEls: ${bokserEls}`)
+let levelEl = document.querySelector('#level')
+
+
+
+
 
 
 
 
 let level=1
 
-let klikkeBokser = level+2
-console.log(`Klikkebokser ${klikkeBokser}`)
-let hviteBokser =[]
+/* let klikkeBokser = level+2   //antall bokser som blir hvite
+    console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`) */
 
+let hviteBokser =[] 
 
-/* while(hviteBokser.length<klikkeBokser){
-    let a=Math.floor(Math.random()*bokserEls.length)
-
-    console.log(`før: ${a}`)
-    if(!hviteBokser.includes(a)){
-    console.log(`etter: ${a}`)
-    hviteBokser.push(a)
-    }
-} */
-
-/* while(hviteBokser.length<klikkeBokser){
-    let a=Math.floor(Math.random()*bokserEls.length)
-    let boksEl=bokserEls[a]
-    console.log(`før: ${boksEl}`)
-    if(!hviteBokser.includes(boksEl)){
-    console.log(`etter: ${boksEl}`)
-    hviteBokser.push(boksEl)
-    }
-} */
 buttonEl.addEventListener('click',blinking)
 
 function blinking(){
+    
+
+    let klikkeBokser = level+2   //antall bokser som blir hvite
+    console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`)
+
+
+    
+
+    buttonEl.style.display = 'none'
+    //levelEl.innerHTML=`${level}`
+
     while(hviteBokser.length<klikkeBokser){
         let a=Math.floor(Math.random()*bokserEls.length)
-        console.log(`før: ${bokserEls[a]}`)
         if(!hviteBokser.includes(bokserEls[a])){
-        console.log(`etter: ${bokserEls[a]}`)
         hviteBokser.push(bokserEls[a])
         }
     }
@@ -67,7 +63,7 @@ function blinking(){
 
 
 
-    console.log(`bokser i array: ${hviteBokser}`)
+    
 
 
 
@@ -86,44 +82,121 @@ function blinking(){
     setTimeout(function(){
     for(i=0;i<hviteBokser.length;i++){
         hviteBokser[i].style.backgroundColor="rgba(0, 0, 0, 0.153)"}
-        hviteBokser = []
+        /* hviteBokser = [] */
     },1000)
 
 
 
 
 
+    } // slutter blinkbokser
+    
 
 
-        
 
-    //blinkBokser[g].style.backgroundColor = 'rgba(90, 90, 90, 0.3)'
+for(h=0;h<bokserEls.length;h++){
+    bokserEls[h].addEventListener('click',valgt)
+}
+let valgteBokser = []
+function valgt(){
+    
+    if(hviteBokser.includes(this)){
+        this.style.backgroundColor='white'
+        if(!valgteBokser.includes(this)){
+            valgteBokser.push(this)
+        }
 
+        if(hviteBokser.length===valgteBokser.length){
 
-    //if (!tallIndekser.includes(tilfeldigIndeks)) 
+            levelFlash()
+            setTimeout(function(){
+                for(i=0;i<valgteBokser.length;i++){
+                    valgteBokser[i].style.backgroundColor="rgba(0, 0, 0, 0.153)"
+                    
+                }
+                valgteBokser = []
+                hviteBokser = []
+            },500)
+            setTimeout(function(){
+                levelEl.innerHTML=`${level}`
+            },1000)
+            
+            setTimeout(blinking,1400)
+            
+    
+        }
 
-
-    /* function write(){
+    }
+    else if(!hviteBokser.includes(this)){
         document.write(`<h1 style="color: black;
         text-align: center;
         font-size: 22px;
         padding-top: 125px;
         font-family: Helvetica, Arial, sans-serif;
-        ">Sqeuence Memory</h1>`)
+        ">Feil!!!</h1>`)
+        console.log(`Du klikket på feil boks og tapte, du nådde level:${level}`)
 
-
-
-    } */
-
-
-
-
-
-    console.log(bokserEls)
+        
+    }
+        
+        
+        
     
 }
 
 
 
+/* } */ // slutter blinkbokser
+
+function levelFlash(){
+    console.log('Du har klikket på alle de rikgtige boksene')
+    
+    
+        level +=1
+        console.log(`nåværende level: ${level}`)
+        document.body.style.transition = 'background-color 1s ease'
+        document.body.style.backgroundColor = 'rgb(70, 190, 255)'
+        setTimeout(function(){document.body.style.backgroundColor = 'rgb(43, 135, 209)'
+        console.log('endrer farge tilbake nå') 
+    }, 350)
+}
+
+
+
+//document.body.style.backgroundColor = 'rgb(70, 190, 255)'
+//                setTimeout(function(){document.body.style.backgroundColor = 'rgb(43, 135, 209)'}, 350)
+
+
 /* neste:  
 gjøre klikking mulig og gjøre at om man klikker feil / riktig får man feilmeldig/nytt level  */
+
+/* neste: 
+øke areal på visse leveler level 3, 6 og 9 eller no
+gjøre at skjer innefor en loop slik at man ikke kan klikke på ting før
+antall bokser som blinker skal øke med 1 hver gang
+
+*/
+
+
+
+/* funker ikke når alt er innenfor en funksjon - vil ha sånn at jeg kan klikke på bokser før start
+
+hvorfor dobbler level da?
+
+det funker ikke når det ikke er over 9 bokser. 
+
+
+
+nå slutter den halveis, men 
+
+let klikkeBokser = level+2   //antall bokser som blir hvite
+    console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`)
+
+
+er innenfor funskjonen
+
+om dette er utenfor stiger ikke klikkbokser og det funker evig med 3 bokser.
+
+når innefor som nå. funker fint til 9 bokser
+*/
+
