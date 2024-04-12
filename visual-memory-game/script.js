bdfa775d507aea53e7e32fbab24f60a0c0c743da
FrithjofSelvaag <frstse@gmail.com>
2024-04-12 08:26:08 +0200

    kok

--- visual-memory-game/script.js
+++ visual-memory-game/script.js
@@ -18,8 +18,8 @@ bokser eller ved å linke til en ny side jeg opierrer over til.
 
 må også lage skjerm jeg kommer til ved tap - kan kopiere litt fra henrik her. 
 */
-
 let bokserEls=document.querySelectorAll('div div')
+/* let bokserEls=document.querySelectorAll('div div') */
 let buttonEl=document.querySelector('#StartKnapp')
 console.log(`lengde på bokserEls array: ${bokserEls.length}`)
 console.log(`bokserEls: ${bokserEls}`)
@@ -32,25 +32,62 @@ let levelEl = document.querySelector('#level')
 
 
 
-let level=1
+
 
 /* let klikkeBokser = level+2   //antall bokser som blir hvite
     console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`) */
 
+
+let level=1
 let hviteBokser =[] 
 
-buttonEl.addEventListener('click',blinking)
+buttonEl.addEventListener('click',startSpill)
 
-function blinking(){
+
+
+
+function startSpill(){
+    buttonEl.style.display = 'none'
+
+    for(h=0;h<bokserEls.length;h++){
+        bokserEls[h].addEventListener('click',valgt)
+    }
+
+    nesteLevel()
+}
+
+
+
+
+
+
+
+function nesteLevel(){
     
 
+
+
+    
     let klikkeBokser = level+2   //antall bokser som blir hvite
     console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`)
 
+// mulig å lage noe sånnt som ,if level, 3, 6,10, 15 ,2...+5+5+ antall == roten av tall + 1 ^^2 
+if (level === 3) {
+
+    for(i=0;i<bokserEls.length;i++){
+        removeElem(bokserEls[i])
+    }
+    for (i = 0;i<16;i==0){
+        let nyBoks=docuemnt.createElement('div')
+        bokserEls.appendChild('nyBoks')
+    }
+    let tilesEl = docuemnt.querySelector('.tiles')
+    tilesEl.style.gridTemplateColumns = `repeat(4, 120px)`; 
+}
 
     
 
-    buttonEl.style.display = 'none'
+    
     //levelEl.innerHTML=`${level}`
 
     while(hviteBokser.length<klikkeBokser){
@@ -76,13 +113,17 @@ function blinking(){
                                                             //begge disse funker - bruke øverste for å vise kompetanse. nedreste er mer forstålig
     for(i=0;i<hviteBokser.length;i++){
         hviteBokser[i].style.backgroundColor="white"
+        hviteBokser[i].classList.add("flipped")
         
     }
 
     setTimeout(function(){
     for(i=0;i<hviteBokser.length;i++){
-        hviteBokser[i].style.backgroundColor="rgba(0, 0, 0, 0.153)"}
-        /* hviteBokser = [] */
+        hviteBokser[i].style.backgroundColor="rgba(0, 0, 0, 0.153)"
+        hviteBokser[i].classList.remove("flipped")
+    }
+        
+        
     },1000)
 
 
@@ -94,14 +135,23 @@ function blinking(){
 
 
 
-for(h=0;h<bokserEls.length;h++){
+/* for(h=0;h<bokserEls.length;h++){
     bokserEls[h].addEventListener('click',valgt)
-}
+} */
+
+
+
+
+
+
+
+
 let valgteBokser = []
 function valgt(){
     
     if(hviteBokser.includes(this)){
         this.style.backgroundColor='white'
+        this.classList.add("flipped")
         if(!valgteBokser.includes(this)){
             valgteBokser.push(this)
         }
@@ -112,6 +162,7 @@ function valgt(){
             setTimeout(function(){
                 for(i=0;i<valgteBokser.length;i++){
                     valgteBokser[i].style.backgroundColor="rgba(0, 0, 0, 0.153)"
+                    valgteBokser[i].classList.remove("flipped")
                     
                 }
                 valgteBokser = []
@@ -121,28 +172,189 @@ function valgt(){
                 levelEl.innerHTML=`${level}`
             },1000)
             
-            setTimeout(blinking,1400)
+            setTimeout(nesteLevel,1400)
             
     
         }
 
-    }
-    else if(!hviteBokser.includes(this)){
-        document.write(`<h1 style="color: black;
-        text-align: center;
-        font-size: 22px;
-        padding-top: 125px;
-        font-family: Helvetica, Arial, sans-serif;
-        ">Feil!!!</h1>`)
-        console.log(`Du klikket på feil boks og tapte, du nådde level:${level}`)
 
-        
     }
+
+
+
+
+
+
+    else if(!hviteBokser.includes(this)){
+        document.write(`
+            <h1 style="color: white;
+            text-align: center;
+            font-size: 22px;
+            padding-top: 125px;
+            font-family: Helvetica, Arial, sans-serif;
+            ">Visual Memory</h1>
+            <h1 style="color: white;
+            text-align: center;
+            margin-top: 0px;
+            margin-bottom: 0px;
+            font-size: 100px;
+            padding-top: 0px;
+            font-family: Helvetica, Arial, sans-serif;
+            ">Level ${level}</h1>
+            <h1 style="color: white;
+            text-align: center;
+            font-size: 22px;
+            padding-top: 0px;
+            font-family: Helvetica, Arial, sans-serif;
+            ">Better Luck Next Time</h1>
+            `)
+
+            let nyKnapp = document.createElement('button')  //prøv igjenn knapp
+            nyKnapp.textContent = ('Try Again')
+            nyKnapp.classList.add('knapp')
+            document.body.appendChild(nyKnapp)
+
+            let nyKnapp2 = document.createElement('button')
+            nyKnapp2.textContent = ('Home')
+            nyKnapp2.classList.add('knapp')
+            document.body.appendChild(nyKnapp2)
+
+            let nyKnapp3 = document.createElement('button')
+            nyKnapp3.textContent = ('Save Score')
+            nyKnapp3.classList.add('knapp')
+            document.body.appendChild(nyKnapp3)
+
+            nyKnapp.addEventListener('mouseenter', function() {
+                nyKnapp.style.backgroundColor = 'white'
+            })
+            nyKnapp.addEventListener('mouseleave', function() {
+                nyKnapp.style.backgroundColor = 'rgb(254, 217, 32)'
+            })
+            nyKnapp.addEventListener('click', function(){
+                location.reload()
+            })
+            nyKnapp2.addEventListener('mouseenter', function() {
+                nyKnapp2.style.backgroundColor = 'white'
+            })
+            nyKnapp2.addEventListener('mouseleave', function() {
+                nyKnapp2.style.backgroundColor = 'rgba(255, 255, 255, 0.400)'
+            })
+            nyKnapp2.addEventListener('click', function(){
+                window.location.href = '../Hovedside/index.html'
+            })
+            nyKnapp3.addEventListener('mouseenter', function() {
+                nyKnapp3.style.backgroundColor = 'white'
+            })
+            nyKnapp3.addEventListener('mouseleave', function() {
+                nyKnapp3.style.backgroundColor = 'rgb(254, 217, 32)'
+            })
+            nyKnapp.style.display = 'flex'
+            nyKnapp.style.margin = 'auto'
+            nyKnapp.style.marginTop = '20px'
+            nyKnapp.style.width = '160px'
+            nyKnapp.style.height = '50px'
+            nyKnapp.style.justifyContent = 'center'
+            nyKnapp.style.alignItems = 'center'
+            nyKnapp.style.fontWeight = '40px'
+            nyKnapp.style.border = 'none'
+            nyKnapp.style.borderRadius = '5px'
+            nyKnapp.style.transition = 'background-color 0.5s ease'
+            nyKnapp.style.fontSize = '25px'
+            nyKnapp.style.fontFamily = 'Helvetica, Arial, sans-serif'
+
+            nyKnapp2.style.display = 'flex'
+            nyKnapp2.style.margin = '20px auto'
+            nyKnapp2.style.width = '160px'
+            nyKnapp2.style.height = '50px'
+            nyKnapp2.style.justifyContent = 'center'
+            nyKnapp2.style.alignItems = 'center'
+            nyKnapp2.style.fontWeight = '40px'
+            nyKnapp2.style.border = 'none'
+            nyKnapp2.style.borderRadius = '5px'
+            nyKnapp2.style.transition = 'background-color 0.5s ease'
+            nyKnapp2.style.fontSize = '25px'
+            nyKnapp2.style.fontFamily = 'Helvetica, Arial, sans-serif'
+
+            nyKnapp3.style.display = 'flex'
+            nyKnapp3.style.margin = '20px auto'
+            nyKnapp3.style.width = '160px'
+            nyKnapp3.style.height = '50px'
+            nyKnapp3.style.justifyContent = 'center'
+            nyKnapp3.style.alignItems = 'center'
+            nyKnapp3.style.fontWeight = '40px'
+            nyKnapp3.style.border = 'none'
+            nyKnapp3.style.borderRadius = '5px'
+            nyKnapp3.style.transition = 'background-color 0.5s ease'
+            nyKnapp3.style.fontSize = '25px'
+            nyKnapp3.style.fontFamily = 'Helvetica, Arial, sans-serif'
+            nyKnapp3.style.backgroundColor = 'rgb(254, 217, 32)'
+
+            nyKnapp.style.backgroundColor = 'rgb(254, 217, 32)'
+                nyKnapp2.style.backgroundColor = 'rgba(255, 255, 255, 0.400)'
+                document.body.style.transition = 'background-color 1s ease'
+                setTimeout(function(){document.body.style.backgroundColor = 'red'}, 10)
+                setTimeout(function(){document.body.style.backgroundColor = 'rgb(43, 135, 209)'}, 200)
+
+                nyKnapp3.addEventListener('click', function(){
+                    document.body.innerHTML = ""
+                    document.write(`
+                    <h1 style="color: white;
+                    text-align: center;
+                    font-size: 22px;
+                    padding-top: 125px;
+                    font-family: Helvetica, Arial, sans-serif;
+                    ">Visual Memory</h1>
+                    <h1 style="color: white;
+                    text-align: center;
+                    margin-top: 0px;
+                    margin-bottom: 0px;
+                    font-size: 100px;
+                    padding-top: 0px;
+                    font-family: Helvetica, Arial, sans-serif;
+                    ">Level ${level}</h1>
+                    <h1 style="color: white;
+                    text-align: center;
+                    font-size: 22px;
+                    padding-top: 0px;
+                    font-family: Helvetica, Arial, sans-serif;
+                    ">Write Your Name</h1>
+                    `)
+                    let labelEl = document.createElement('label')
+                    labelEl.classList.add('label')
+                    document.body.appendChild(labelEl)
+                    let inputEl = document.createElement('input')
+                    inputEl.classList.add('input')
+                    labelEl.appendChild(inputEl)
+
+                    labelEl.style.color = 'white'
+                    labelEl.style.display = 'flex'
+                    labelEl.style.margin = 'auto'
+                    labelEl.style.justifyContent = 'center'
+                
+                    inputEl.addEventListener('keydown', function(e) {
+                        if (e.key === 'Enter') {
+                            let playerName = String(inputEl.value)
+                            let highscore = level
+                            localStorage.setItem('highscore', highscore)
+                            localStorage.setItem('playerName', playerName)
+                            console.log(localStorage.getItem('playerName'))
+                            console.log(localStorage.getItem('highscore'))
+                            window.location.href = '../leaderboard/leaderboard.html'
+                    }
+                })
+        
+        })
         
         
         
     
 }
+}
+
+
+
+
+
 
 
 
