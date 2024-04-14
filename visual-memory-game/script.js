c0e2b006aab76a04c19d58ce970b9ffeb9e85bd5
FrithjofSelvaag <frstse@gmail.com>
2024-04-14 20:04:50 +0200

    kok

--- visual-memory-game/script.js
+++ visual-memory-game/script.js
@@ -1,42 +1,15 @@
-//gjøre:
-/*En funskjon som kjører hver gang et nytt level nås. Denne funskjonen gjør at et antall bokser blir hvite og så tilbake til fagen etter et par
-sekunder. 
-
-Etter dette har skjedd kan du trykke på boksene. Lag en for-løkke innenfor click funskjonen på boksene. Går igejnnom arrayet med boksene som 
-ble hvite. i++ i<x.length i+1..... Slutter om den finner en boks som stemmer. Om den ikke gjør de tapper du. om den gjør det blir boksen hvit. 
-boksen må da tas ut av løkken slik at den ikke blir hvit igjenn og legges inn i et annet array der den ikke endres når trykkes på.
-
-når alle boksene som stemmer er tatt. kan ta når det arrayet som får riktige bokser er like langt som det som ikke får like bokser. 
-kommer nytt level og vi legger en boks til inn i rekkefølgen. 
-
-
-når vi når et visst nivå må jeg legge til slik at vi går fra 9 til 16, så 25 bokser. enneten ved docuemnt.write også skrive alt inn, men med flere
-bokser eller ved å linke til en ny side jeg opierrer over til.
-
-
-må også lage skjerm jeg kommer til ved tap - kan kopiere litt fra henrik her. 
-*/
-
-/* let bokserEls=document.querySelectorAll('div div') */
 
 
 
 let buttonEl=document.querySelector('#StartKnapp')
-/* console.log(`lengde på bokserEls array: ${bokserEls.length}`)
-console.log(`bokserEls: ${bokserEls}`) */
+
 let levelEl = document.querySelector('#level')
-
-
 
 
 let erIOvergang = false
 
 
 
-
-
-/* let klikkeBokser = level+2   //antall bokser som blir hvite
-    console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`) */
 let tilesEl=document.querySelector('.tiles')
 let bokserEls=document.querySelectorAll('.tiles > div')
 
@@ -59,22 +32,11 @@ function lagBokser(n){
 
     
 }
-
-
-
-
-
-
 lagBokser(9)
-
-
 
 console.log(`BokserEls: ${bokserEls}`)
 console.log(bokserEls)
 console.log(`BokserEls lengde: ${bokserEls.length}`)
-
-
-
 
 
 function startSpill(){
@@ -90,11 +52,6 @@ function startSpill(){
 }
 
 
-
-
-
-
-
 function nesteLevel(){
     
     
@@ -106,14 +63,6 @@ function nesteLevel(){
 
 
 
-    // mulig å lage noe sånnt som ,if level, 3, 6,10, 15 ,2...+5+5+ antall == roten av tall + 1 ^^2 
-
-
-    
-    
-
-    
-    //levelEl.innerHTML=`${level}`
 
     while(hviteBokser.length<klikkeBokser){
         let a=Math.floor(Math.random()*bokserEls.length)
@@ -124,19 +73,7 @@ function nesteLevel(){
     }
 
 
-
-
-    
-
-
-
-
-    /* function blirHvit(boks) {
-        boks.style.backgroundColor = 'white';
-    }
-
-    hviteBokser.forEach(element => blirHvit(element)); */
-                                                            //begge disse funker - bruke øverste for å vise kompetanse. nedreste er mer forstålig
+                                                   
     for(i=0;i<hviteBokser.length;i++){
         hviteBokser[i].style.backgroundColor="white"
         hviteBokser[i].classList.add("flipped")
@@ -156,15 +93,8 @@ function nesteLevel(){
 
 
 
-    } // slutter blinkbokser
+} 
     
-
-
-
-/* for(h=0;h<bokserEls.length;h++){
-    bokserEls[h].addEventListener('click',valgt)
-} */
-
 
 
 
@@ -236,7 +166,7 @@ function valgt(){
                     bokserEls.forEach(function(boks){
                         boks.style.height = '90px'
                     })
-                    // I fix Joffen :)
+                    // I fix Joffen :)   <3
             
                     
                 }if(level>=6){
@@ -283,8 +213,8 @@ function valgt(){
 
 
                 
-                bokserEls=document.querySelectorAll('.tiles > div') //hvorfor må dette defineres igjenn. sameme greia over før jeg kunne console.logge 
-                                                                    //bokserEls
+                bokserEls=document.querySelectorAll('.tiles > div') 
+
                 for(h=0;h<bokserEls.length;h++){
                     bokserEls[h].addEventListener('click',valgt)
                 }
@@ -347,7 +277,7 @@ function valgt(){
             levelFlashTap()
             
             
-            //heeerrr
+            
             //resterter levelet.
 
                 if (livTapt.length==1){ 
@@ -581,7 +511,6 @@ function valgt(){
 
 
 }
-/* } */ // slutter blinkbokser
 
 function levelFlash(){
     console.log('Du har klikket på alle de rikgtige boksene')
@@ -611,50 +540,15 @@ function levelFlashTap(){
 }
 
 
-//document.body.style.backgroundColor = 'rgb(70, 190, 255)'
-//                setTimeout(function(){document.body.style.backgroundColor = 'rgb(43, 135, 209)'}, 350)
-
-
-/* neste:  
-gjøre klikking mulig og gjøre at om man klikker feil / riktig får man feilmeldig/nytt level  */
-
-/* neste: 
-øke areal på visse leveler level 3, 6 og 9 eller no
-gjøre at skjer innefor en loop slik at man ikke kan klikke på ting før
-antall bokser som blinker skal øke med 1 hver gang
-
-*/
-
-/* helgen: 
- fullføre boks økning
- liv
- 3 feil før liv
- feil blir mørkeblå
- font
-
-*/
 
 
 
-/* funker ikke når alt er innenfor en funksjon - vil ha sånn at jeg kan klikke på bokser før start
 
-hvorfor dobbler level da?
-
-det funker ikke når det ikke er over 9 bokser. 
+ 
 
 
-
-nå slutter den halveis, men 
-
-let klikkeBokser = level+2   //antall bokser som blir hvite
-    console.log(`Klikkebokser(antall bokser som skal lyse): ${klikkeBokser}`)
-
-
-er innenfor funskjonen
-
-om dette er utenfor stiger ikke klikkbokser og det funker evig med 3 bokser.
-
-når innefor som nå. funker fint til 9 bokser
+/*
+fiksa slik at man kan holde på evig eller stopper på et nivår
 
 endre fra i = 0 til let i = 0 i for løkker
 */
